7e2bfc79c904c0910fda895fde234cb5e45216ed
Roy Dadon <92038509+rroyd@users.noreply.github.com>
2024-12-02 17:56:02 +0200

    Initial Commit

--- Ex01_01/Program.cs
+++ Ex01_01/Program.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ex01_01
+﻿namespace Ex01_01
 {
-    internal class Program
+    class Program
     {
+
     }
 }
