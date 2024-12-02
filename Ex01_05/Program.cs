71104e218bd6530e922b46bd417d3f0aa81b9b7e
sahar-aviad <saharaviad714@gmail.com>
2024-12-02 19:46:20 +0200

    sahar work

--- Ex01_05/Program.cs
+++ Ex01_05/Program.cs
@@ -1,6 +1,11 @@
-﻿namespace Ex01_05
+﻿using System;
+namespace Ex01_05
 {
     class Program
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("sahar");
+        }
     }
 }
